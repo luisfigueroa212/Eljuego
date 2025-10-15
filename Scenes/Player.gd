a62d78336fec5599e0e4e0d281447ed84d8e7f17
luisfigueroa212 <micaracuando44@gmail.com>
2025-10-15 20:48:55 -0300

    Borrar linea

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -88,7 +88,5 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 		# Restaura la velocidad normal
 		current_speed = normal_speed
 		
-
-
 func _on_cambio_escenario_body_entered(body: Node2D) -> void:
 	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
