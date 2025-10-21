30d9583ea16aa9cb403877602534893f90ebb48b
luisfigueroa212 <micaracuando44@gmail.com>
2025-10-21 19:25:30 -0300

    Escena bosque lista

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_button_jugar_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/game.tscn")
+	get_tree().change_scene_to_file("res://Scenes/bosque.tscn")
 	
 func _on_button_salir_pressed() -> void:
 	get_tree().quit()
