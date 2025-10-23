a250c447427b17ee4920eeeebe249bdba9919700
luisfigueroa212 <micaracuando44@gmail.com>
2025-10-23 00:17:30 -0300

    Listo

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -88,5 +88,7 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 		# Restaura la velocidad normal
 		current_speed = normal_speed
 		
+
+
 func _on_cambio_escenario_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://Scenes/scene2.tscn")
+	get_tree().change_scene_to_file("res://Scenes/elcorazon.tscn")
