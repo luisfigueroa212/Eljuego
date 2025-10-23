fa025e93e859b7b814161b30456c60bee2cdb040
luisfigueroa212 <micaracuando44@gmail.com>
2025-10-23 00:37:36 -0300

    terminado

--- Scripts/Menu de inicio/musica_menu.gd
+++ Scripts/Menu de inicio/musica_menu.gd
@@ -1,5 +1,17 @@
 extends AudioStreamPlayer
 
-#Sonido en bucle
-func _on_finished():
-	self.play()
+const RETRASO_SEGUNDOS = 1 
+
+var tiempo_transcurrido = 0.0
+
+func _process(delta):
+	# Incrementa el tiempo transcurrido con el tiempo delta de cada frame
+	tiempo_transcurrido += delta
+	
+	# Verifica si el tiempo transcurrido es mayor o igual al retraso deseado
+	if tiempo_transcurrido >= RETRASO_SEGUNDOS:
+		# Llama a la función 'play()' para que el audio comience a sonar
+		play()
+		
+		# Detiene el procesamiento de esta lógica una vez que el audio se reproduce
+		set_process(false)
